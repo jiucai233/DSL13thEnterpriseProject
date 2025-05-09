1b80fc3481659cea2ecc992e91f539450553f77f
jiucai233 <jiucai@yonsei.ac.kr>
2025-05-09 10:18:02 +0900

    ♻️ refactor(inference): update yolo inference script
    - comment out video inference section for clarity
    - update paths to use relative paths instead of absolute paths
    - remove deprecated files from the repository

--- YOLO_inference.py
+++ YOLO_inference.py
@@ -6,33 +6,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from ultralytics import YOLO
 
-model = YOLO('D:/my_codes/DSL13thEnterpriseProject/runs/detect/train/weights/best.pt')#change!!
-results = model.predict(
-    source='D:/my_codes/DSL13thEnterpriseProject/data/raw_data/clear_vid_test_box2.mp4',#change!!
-    show=True,
-    save=True,  
-    conf=0.5,   
-)
-# annotated_img = results[0].plot()
+# # this section is for testing the model on a video
+# model = YOLO('runs/detect/train/weights/best.pt') #change!!
+# results = model.predict(
+#     source='data/raw_data/clear_vid_test_box2.mp4', #change!!
+#     show=True,
+#     save=True,  
+#     conf=0.5,   
+# )
 
-
-# plt.imshow(annotated_img[:, :, ::-1])  # BGR2RGB
-# plt.axis('off')
-# plt.show()
-
-
-
-# # Plot the training and validation loss
-# # Load the training results from the CSV file
-# # Note: Make sure to change the path to your results.csv file
-# data = pd.read_csv("D:/my_codes/DSL13thEnterpriseProject/runs/detect/train/results.csv")
-# plt.figure(figsize=(10, 6))
-# plt.title('Training and Validation Loss')
-# plt.xlabel('Epoch')
-# plt.ylabel('Loss')
-# plt.plot(data['epoch'], data['val/box_loss'], label='val/box_loss', marker='x')
-# plt.plot(data['epoch'], data['val/cls_loss'], label='val/cls_loss', marker='x')
-# plt.plot(data['epoch'], data['train/box_loss'], label='train/box_loss', marker='o')
-# plt.plot(data['epoch'], data['train/cls_loss'], label='train/cls_loss', marker='o')
-# plt.legend()
-# plt.show()
+# Plot the training and validation loss
+# Load the training results from the CSV file
+# Note: Make sure to change the path to your results.csv file
+data = pd.read_csv("runs/detect/train/results.csv")
+plt.figure(figsize=(10, 6))
+plt.title('Training and Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.plot(data['epoch'], data['val/box_loss'], label='val/box_loss', marker='x')
+plt.plot(data['epoch'], data['val/cls_loss'], label='val/cls_loss', marker='x')
+plt.plot(data['epoch'], data['train/box_loss'], label='train/box_loss', marker='o')
+plt.plot(data['epoch'], data['train/cls_loss'], label='train/cls_loss', marker='o')
+plt.legend()
+plt.show()
